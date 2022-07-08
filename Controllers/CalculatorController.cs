78a2a4c0c6a47bd5d3fa1e0fc8ac8132483bddd9
Simon Kurtz <simonkurtz@microsoft.com>
2022-07-08 14:54:12 -0400

    Add basic calculator methods

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : Controller
+    {
+
+        #region Public Actions
+
+        [HttpGet, HttpPost]
+        [Route("[action]")]
+        public ActionResult<decimal> Add(decimal addend1, decimal addend2)
+        {
+            return Ok(addend1 + addend2);
+        }
+
+        [HttpGet, HttpPost]
+        [Route("[action]")]
+        public ActionResult<decimal> Divide(decimal dividend, decimal divisor)
+        {
+            if (divisor != 0)
+            {
+                return Ok(dividend / divisor);
+            } 
+            else
+            {
+                return BadRequest("Cannot divide by 0."); 
+            }
+
+        }
+
+        [HttpGet, HttpPost]
+        [Route("[action]")]
+        public ActionResult<decimal> Multiply(decimal factor1, decimal factor2)
+        {
+            return Ok(factor1 * factor2);
+        }
+
+        [HttpGet, HttpPost]
+        [Route("[action]")]
+        public ActionResult<decimal> Subtract(decimal minuend, decimal subtrahend)
+        {
+            return Ok(minuend - subtrahend);
+        }
+
+        #endregion
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CalculatorApi
+
+This is a very simple Calculator Web API written in .NET 6: https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-6.0&tabs=visual-studio
